dc74759ecacc3e009fa29d4344134aff8dceae47
123jjck <48408304+123jjck@users.noreply.github.com>
2019-03-26 07:54:20 +0300

    FIX FCKIN TABLE NAME

--- api/ban.php
+++ api/ban.php
@@ -5,7 +5,7 @@ $db = new mysqli('localhost', 'root', '', 'daisy');
 
 
 if (isset($_POST["ticket"])) {
-	$user = $db->query("SELECT * FROM USERS WHERE TICKET = '" . $_POST["ticket"] . "';");
+	$user = $db->query("SELECT * FROM users WHERE TICKET = '" . $_POST["ticket"] . "';");
 
 	if ($user->num_rows <= 0) {
 		exit;
@@ -13,6 +13,7 @@ if (isset($_POST["ticket"])) {
 
 	$a = $user->fetch_assoc();
 	if ($a['ROLEFLAGS'] == "393230") {
-		$q = $db->query("UPDATE USERS SET ISBANNED = 1 WHERE ID = " . $_POST["id"] . ";");
+		$q = $db->query("UPDATE users SET ISBANNED = 1 WHERE ID = " . $_POST["id"] . ";");
 	}
 }
+?>
