f00c87ac9e6f5fe1a3295e4cbd869c2ed3a23013
LowderPlay <lowderchannel@gmail.com>
2019-04-30 23:05:07 +0500

    Неправильное название таблицы

--- api/setAvatar.php
+++ api/setAvatar.php
@@ -3,4 +3,4 @@
 
 $db = new mysqli('localhost', 'root', '', 'daisy');
 
-$q = $db->query("UPDATE users SET AVATAR  = '" . $_POST['avatar'] . "' WHERE TICKET = '" . $_POST['ticket'] ."';");
+$q = $db->query("UPDATE USERS SET AVATAR  = '" . $_POST['avatar'] . "' WHERE TICKET = '" . $_POST['ticket'] ."';");
