2766fa9d0dd90b904b00aec3d2d3493c991c4f0a
123jjck <48408304+123jjck@users.noreply.github.com>
2019-05-05 09:59:04 +0500

    Update bg.php

--- api/bg.php
+++ api/bg.php
@@ -1,11 +1,5 @@
 <?php
-
 $dbh = new PDO("mysql:host=localhost;dbname=daisy;charset=UTF8", 'root', '');
-
-if (strlen($_POST["bg"]) > 5) { // проверка на дурачка
-exit();
-}
-
 $q = $dbh->prepare("UPDATE users SET BG  = :bg WHERE TICKET = :token");
  $q->execute(array('bg' => $_POST['bg'], 'token' => $_POST['ticket']));
 ?>
