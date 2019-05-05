63ff20d62234b91fb27d4d28ae2ef7ea5a651416
123jjck <48408304+123jjck@users.noreply.github.com>
2019-05-05 14:44:08 +0500

    Update kek.php

--- api/kek.php
+++ api/kek.php
@@ -1,10 +1,8 @@
 <?php
 header('Content-Type: text/plain; charset=utf-8');
-$db = new mysqli('localhost', 'root', '', 'daisy');
-$db->set_charset("utf8");
-
-$q = $db->query("SELECT * FROM USERS WHERE TICKET = '" . $_GET['ticket'] ."';");
-
-$a = $q->fetch_assoc();
-
+include("/var/www/html/db_connection.php");
+global $dbh;
+	$q = $dbh->prepare("SELECT * FROM users WHERE TICKET = :ticket");
+			$q->execute(array('ticket' => $_GET['ticket']));
+				$a = $q->fetch(PDO::FETCH_ASSOC);
 echo "id=" . $a['ID']  . "&username=" . $a['USERNAME']  . "&level=" . $a['LEVEL'] . "&regdate=" . $a['REGDATE'] . "&roleflags=" . $a['ROLEFLAGS'] . "&money=".$a["MONEY"]."&gold=".$a["GOLD"]."&magic=".$a["MAGIC"]."&avatar=" . $a['AVATAR'] . "&inventory=" . $a['INVENTORY'] . "&isbanned=" . $a['ISBANNED'] . "&bg=" . $a['BG'];
