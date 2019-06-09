5820cba4b66837b25b7a5550009fdcd8eb241104
123jjck <48408304+123jjck@users.noreply.github.com>
2019-06-09 19:04:21 +0500

    fix bg saving

--- api/bginv.php
+++ api/bginv.php
@@ -3,6 +3,11 @@
 $dbh = new PDO("mysql:host=localhost;dbname=daisy;charset=UTF8", 'root', '');
 
 if (isset($_POST["inventory"]) && isset($_POST["ticket"])) {
+	$inv = $_POST["inventory"];
+	$invred = str_replace("IsUsed>0|IsLimited>0|", '', $inv);
+	$invred2 = str_replace("|", ';', $invred);
+	$invred3 = preg_replace("/[^0-9,;]/", '', $invred2);
+	
 	 $q = $dbh->prepare("UPDATE users SET BGInv  = :inventory WHERE TICKET = :token");
-	 $q->execute(array('inventory' => $_POST['inventory'], 'token' => $_POST['ticket']));
+	 $q->execute(array('inventory' => $invred3, 'token' => $_POST['ticket']));
 	 }
