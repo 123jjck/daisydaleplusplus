881688739ca8444e2458f447e57904274d8c55b6
123jjck <48408304+123jjck@users.noreply.github.com>
2019-06-09 19:08:03 +0500

    fix nose saving

--- api/setAvatar.php
+++ api/setAvatar.php
@@ -1,5 +1,21 @@
 <?php
 $dbh = new PDO("mysql:host=localhost;dbname=daisy;charset=UTF8", 'root', '');
+	$ava = $_POST['avatar'];
+	$avared = preg_replace("/[^0-9]/", ';', strval($ava));
+	$avacolors = explode(";", $avared);
+	foreach ($avacolors as $color) {
+		if(strlen($color) >= 6 && $color != 16762375) {
+    $goodcolor = $color;
+  }
+}
 
+if(isset($GLOBALS["goodcolor"])) {
+$bestcolor = 'Color>';
+$bestcolor .= $GLOBALS["goodcolor"];
+  $avaecho = str_replace("Color>16762375", $bestcolor, $ava);
 $q = $dbh->prepare("UPDATE users SET AVATAR  = :avatar WHERE TICKET = :token");
- $q->execute(array('avatar' => $_POST['avatar'], 'token' => $_POST['ticket']));
+ $q->execute(array('avatar' => $avaecho, 'token' => $_POST['ticket']));
+} else {
+ $q = $dbh->prepare("UPDATE users SET AVATAR  = :avatar WHERE TICKET = :token");
+ $q->execute(array('avatar' => $ava, 'token' => $_POST['ticket']));
+}
