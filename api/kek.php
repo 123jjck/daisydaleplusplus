fbc86d4207858987d278bc214f14da55a63bd78e
123jjck <48408304+123jjck@users.noreply.github.com>
2019-06-12 16:18:25 +0500

    merge zer0 code

--- api/kek.php
+++ api/kek.php
@@ -6,4 +6,4 @@ $dbh = new PDO("mysql:host=localhost;dbname=daisy;charset=UTF8", 'root', '');
 				$a = $q->fetch(PDO::FETCH_ASSOC);
 				$inv = $a['BGInv'];
 	$ava = $a['AVATAR'];
-	echo "id=" . $a['ID'] . "&username=" . $a['USERNAME'] . "&level=" . $a['LEVEL'] . "&regdate=" . $a['REGDATE'] . "&roleflags=" . $a['ROLEFLAGS'] . "&money=".$a["MONEY"]."&gold=".$a["GOLD"]."&magic=".$a["MAGIC"]."&avatar=" . $ava . "&inventory=" . $a['INVENTORY'] . "&isbanned=" . $a['ISBANNED'] . "&bg=" . $a['BG'] . "&bginven=" . $inv;
+	echo "id=" . $a['ID'] . "&username=" . $a['USERNAME'] . "&level=" . $a['LEVEL'] . "&regdate=" . $a['REGDATE'] . "&roleflags=" . $a['ROLEFLAGS'] . "&money=".$a["MONEY"]."&gold=".$a["GOLD"]."&magic=".$a["MAGIC"]."&avatar=" . $ava . "&inventory=" . $a['INVENTORY'] . "&isbanned=" . $a['ISBANNED'] . "&bg=" . $a['BG'] . "&bginven=" . $inv . '&body_color='.$a['BODY_COLOR'].'&legs='.$a['LEGS'].'&legs_color='.$a['LEGS_COLOR'].'&ears='.$a['EARS'].'&ears_color='.$a['EARS_COLOR'],'&eyes='.$a['EYES'].'&nose='.$a['NOSE'].'&mouth='.$a['MOUTH'].'&peak='.$a['PEAK'].'&horns='.$a['HORNS'].'&house_id='.$a['HOUSE_ID'].'&house_str='.$a['HOUSE_STR'];
