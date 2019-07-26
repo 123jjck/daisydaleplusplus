2b8fadf0ed1dba95281f7658cc034891790ceedf
123jjck <48408304+123jjck@users.noreply.github.com>
2019-07-26 08:56:45 +0500

    Update money.php

--- api/money.php
+++ api/money.php
@@ -1,5 +1,5 @@
 <?php
 include("db_connection.php"); 
-global $dbh;
+global $db;
 $db->query("UPDATE users SET GOLD=gold-".$_POST['priceRu'].",MONEY=money-".$_POST['priceSm']." WHERE TICKET='".$_POST['ticket']."'");
 ?>
