d0c81fda8c4fc55f7563b37f1c85da1b9db5cd41
123jjck <48408304+123jjck@users.noreply.github.com>
2019-07-26 08:55:20 +0500

    Update addMoney.php

--- api/addMoney.php
+++ api/addMoney.php
@@ -1,4 +1,5 @@
 <?php
-$db = new mysqli('localhost', 'root', '', 'daisy');
+include("db_connection.php"); 
+global $db;
 $db->query("UPDATE users SET MONEY=money+".$_POST['amount']." WHERE TICKET='".$_POST['ticket']."'");
 ?>
