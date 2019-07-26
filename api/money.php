34027ad80531304e9e6e66654be28bb9a26e41d5
123jjck <48408304+123jjck@users.noreply.github.com>
2019-07-26 08:56:28 +0500

    Update money.php

--- api/money.php
+++ api/money.php
@@ -1,4 +1,5 @@
 <?php
-$db = new mysqli('localhost', 'root', '', 'daisy');
+include("db_connection.php"); 
+global $dbh;
 $db->query("UPDATE users SET GOLD=gold-".$_POST['priceRu'].",MONEY=money-".$_POST['priceSm']." WHERE TICKET='".$_POST['ticket']."'");
 ?>
