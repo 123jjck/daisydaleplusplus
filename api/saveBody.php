fc83f36ab3197189f3af5f0277c09531a2b058a0
123jjck <48408304+123jjck@users.noreply.github.com>
2019-07-26 08:57:18 +0500

    Update saveBody.php

--- api/saveBody.php
+++ api/saveBody.php
@@ -1,6 +1,5 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-$db = new mysqli('localhost', 'root', '', 'daisy');
+include("db_connection.php"); 
+global $db;
 $db->query("UPDATE users SET BODY_COLOR=".$_POST['body_color'].",EARS_COLOR=".$_POST['ears_color'].",EARS=".$_POST['ears'].",EYES=".$_POST['eyes'].",HORNS=".$_POST['horns'].",LEGS=".$_POST['legs'].",LEGS_COLOR=".$_POST['legs_color'].",MOUTH=".$_POST['mouth'].",NOSE=".$_POST['nose'].",PEAK=".$_POST['peak']." WHERE TICKET='".$_POST['token']."'");
 ?>
