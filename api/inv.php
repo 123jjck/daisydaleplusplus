3fd76849c35ebc68ce54129cbe9ca97fb59895d2
123jjck <48408304+123jjck@users.noreply.github.com>
2020-01-12 12:16:10 +0500

    Formatted code

--- api/inv.php
+++ api/inv.php
@@ -1,19 +1,15 @@
 <?php
-
-include("db_connection.php"); 
+include ("db_connection.php");
 global $dbh;
 
-if (isset($_POST["inventory"]) && isset($_POST["ticket"])) {
-	 $inv = $dbh->prepare("SELECT INVENTORY FROM users WHERE TICKET = :token");
-	 $inv->execute(array('token' => $_POST['ticket']));
-	 $fetch = $inv->fetch(PDO::FETCH_ASSOC);
-	 $str = $fetch['INVENTORY'];
-   //костыльное ограничение вещей в инвентаре 
-     $count = mb_substr_count($str, '<ID>');
-	 if ($count > 240) {
-     exit();
-     } else {
-	 $q = $dbh->prepare("UPDATE users SET INVENTORY  = :inventory WHERE TICKET = :token");
-	 $q->execute(array('inventory' => $_POST['inventory'], 'token' => $_POST['ticket']));
-	 }
+if (isset($_POST["inventory"]) && isset($_POST["ticket"]))
+{
+    //костыльное ограничение вещей в инвентаре
+    $count = mb_substr_count($_POST['inventory'], '<ID>');
+    if ($count > 240) exit;
+    $q = $dbh->prepare("UPDATE users SET INVENTORY  = :inventory WHERE TICKET = :token");
+    $q->execute(array(
+        'inventory' => $_POST['inventory'],
+        'token' => $_POST['ticket']
+    ));
 }
