f54c5f84db787ee8ad96da2f3d4d37a7dc0d4743
123jjck <48408304+123jjck@users.noreply.github.com>
2020-01-12 11:48:35 +0500

    Fix password vuln.

--- front/db_connection.php
+++ front/db_connection.php
@@ -1,5 +1,27 @@
 <?php
-//mysqli 
-$db = new mysqli('localhost', 'root', '', 'daisy');
-// pdo
-$dbh = new PDO("mysql:host=localhost;dbname=daisy;charset=UTF8", 'root', '');
+/* Данные от базы */
+$host = 'localhost'; // Хост
+$user = 'root'; // Юзер
+$password = ''; // Пароль
+$dbname = 'daisy'; // Название базы данных
+try
+{
+    $db = new mysqli($host, $user, $password, $dbname);
+    if (!$db) throw new Exception('Connection error');
+}
+catch(Exception $e)
+{
+    echo 'Ошибка! Попробуйте зайти в следующий раз!';
+    die;
+}
+
+try
+{
+    $dbh = new PDO("mysql:host=$host;dbname=$dbname;charset=UTF8", $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+    echo 'Ошибка! Попробуйте зайти в следующий раз!';
+    die;
+}
