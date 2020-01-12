9d1f565acdd5ac5a7ebc5e3b38b7c9301aaf9adc
123jjck <48408304+123jjck@users.noreply.github.com>
2020-01-12 12:18:18 +0500

    Formatted code

--- api/ban.php
+++ api/ban.php
@@ -1,21 +1,23 @@
 <?php
 
-
-include("db_connection.php"); 
+include ("db_connection.php");
 global $dbh;
 
-
-if (isset($_POST["ticket"])) {
-	$spkidaly = $dbh->prepare("SELECT * FROM users WHERE TICKET = :ticket");
-	$spkidaly->execute(array('ticket' => $_POST["ticket"]));
-	if (!$spkidaly) {
-		exit;
-	}
-//$id = ($_POST["id"] - 100000);
-	$id = $_POST["id"];
-	$a = $spkidaly->fetch(PDO::FETCH_ASSOC);
-	if ($a['ROLEFLAGS'] >= 131086) {
-		$q = $dbh->prepare("UPDATE users SET ISBANNED = 1 WHERE ID = :id");
-		$q->execute(array('id' => $id));
-	}
+if (isset($_POST["ticket"]))
+{
+    $rolecheck = $dbh->prepare("SELECT ROLEFLAGS FROM users WHERE TICKET = :ticket");
+    $rolecheck->execute(array(
+        'ticket' => $_POST["ticket"]
+    ));
+    $a = $rolecheck->fetch(PDO::FETCH_ASSOC);
+    if (!$a) exit;
+    //$id = ($_POST["id"] - 100000);
+    if ((int)$a['ROLEFLAGS'] >= 131086)
+    {
+        $q = $dbh->prepare("UPDATE users SET ISBANNED = 1 WHERE ID = :id");
+        $q->execute(array(
+            'id' => $_POST['id']
+        ));
+    }
 }
+
