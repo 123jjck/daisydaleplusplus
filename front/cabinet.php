5077f99043d5aeb649f8894024032cdf18a98dc5
123jjck <48408304+123jjck@users.noreply.github.com>
2020-05-07 18:03:59 +0500

    formatted code

--- front/cabinet.php
+++ front/cabinet.php
@@ -1,15 +1,12 @@
 <?php
-    session_start();
-    
-    if (!isset($_SESSION["userId"])) {
-        header("Location: /");
-        exit;
-    }
-    
+session_start();
+if (!isset($_SESSION["userId"])) {
+    header("Location: /");
+}
+
 include("db_connection.php"); 
 global $db;
 $db->set_charset("utf8");
-
 
 $q = $db->query("SELECT * FROM users WHERE ID = " . $_SESSION["userId"] . ";");
 $a = $q->fetch_assoc();
@@ -23,28 +20,26 @@ $regmonth = $date[1];
 $regyear = $date[0];
 
 ?>
-
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8" />
-	<title>Личный кабинет - DD++</title>
-  <link rel="icon" type="image/png" href="/favicon.png" />
-	<link rel="stylesheet" type="text/css" href="/style.css">
+    <title>Личный кабинет - DD++</title>
+    <link rel="icon" type="image/png" href="/favicon.png" />
+    <link rel="stylesheet" type="text/css" href="/style.css">
 </head>
+
 <body>
     <h1>Кабинет</h1>
     <div class="message"></div>
-    
     <form>
         <p>Уровень: <input name="level" value="<?php echo $level; ?>" /></p>
-        <p>Дата регистрации: <input name="regday"  value="<?php echo $regday; ?>"/>.<input name="regmonth"  value="<?php echo $regmonth; ?>"/>.<input name="regyear"  value="<?php echo $regyear; ?>"/></p>
+        <p>Дата регистрации: <input name="regday" value="<?php echo $regday; ?>" />.<input name="regmonth" value="<?php echo $regmonth; ?>" />.<input name="regyear" value="<?php echo $regyear; ?>" /></p>
         <button type="submit">Отправить</button>
     </form>
-    <script
-			  src="https://code.jquery.com/jquery-3.3.1.min.js"
-			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-			  crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script src="/cabinet.js"></script>
 </body>
+
 </html>
