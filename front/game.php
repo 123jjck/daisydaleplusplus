df749c2aa8d3923086aaec5d77acbf78729b67e6
123jjck <48408304+123jjck@users.noreply.github.com>
2020-05-07 18:01:40 +0500

    Formatted code

--- front/game.php
+++ front/game.php
@@ -6,33 +6,32 @@ if (!isset($_SESSION["userId"])) {
 
 $domain = (stripos($_SERVER['SERVER_PROTOCOL'],'https') === 0 ? 'https://' : 'http://') . $_SERVER['HTTP_HOST'] . '/';
 ?>
-
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <title>DD++</title>
     <link type="image/png" href="/favicon.png" rel="icon" />
     <link rel="stylesheet" href="/game.css">
 </head>
-<body>
-    
-         <div class="container">
-            <div class="centered">
-                <embed type="application/x-shockwave-flash" src="base.swf" style="width: 100%" flashvars="<?php echo "game_server=" . $domain . "&url_path_server=" . $domain . "&portal_url=" . $domain . "&manual_server_selection=&start_step=0&useHashInName="; ?>"/>
-            </div>
-        </div>
 
+<body>
+    <div class="container">
+        <div class="centered">
+            <embed type="application/x-shockwave-flash" src="base.swf" style="width: 100%" flashvars="<?php echo "game_server=" . $domain . "&url_path_server=" . $domain . "&portal_url=" . $domain . "&manual_server_selection=&start_step=0&useHashInName="; ?>" />
+        </div>
+    </div>
     <p><a href="/cabinet.php">Кабинет</a></p>
     <p><a href="/logout.php">Выйти</a></p>
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
     <script type="text/javascript">
         function resize_embed() {
-                    $("embed").height(($(window).height() / $(window).width()) * $("embed").width());
+            $("embed").height(($(window).height() / $(window).width()) * $("embed").width());
         }
-
         $(document).ready(resize_embed);
         $(window).resize(resize_embed);
     </script>
 </body>
+
 </html>
