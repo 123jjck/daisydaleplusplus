d9d928719ae14956c2c7fd8275041c21e6efc753
123jjck <48408304+123jjck@users.noreply.github.com>
2020-07-09 23:12:18 +0500

    PDO

--- api/addMoney.php
+++ api/addMoney.php
@@ -1,5 +1,8 @@
 <?php
 include("db_connection.php"); 
-global $db;
-$db->query("UPDATE users SET MONEY=money+".$_POST['amount']." WHERE TICKET='".$_POST['ticket']."'");
+global $dbh;
+$query = $dbh->prepare("UPDATE users SET MONEY= MONEY + :amount WHERE TICKET = :ticket");
+$query->bindParam('amount', $_POST['amount']);
+$query->bindParam('ticket', $_POST['ticket']);
+$query->execute();
 ?>
