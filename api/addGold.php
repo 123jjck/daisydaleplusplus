2c8db93ba9b78eb90a44b8f6f039d3a1d5db6d0a
123jjck <48408304+123jjck@users.noreply.github.com>
2020-07-09 23:10:16 +0500

    PDO

--- api/addGold.php
+++ api/addGold.php
@@ -1,7 +1,7 @@
 <?php
 include("db_connection.php"); 
 global $dbh;
-$query = $dbh->prepare("UPDATE users SET GOLD= GOLD + :amount WHERE TICKET = :ticket'");
+$query = $dbh->prepare("UPDATE users SET GOLD = GOLD + :amount WHERE TICKET = :ticket'");
 $query->bindParam('ticket', $_POST['ticket']);
 $query->bindParam('amount', $_POST['amount']);
 $query->execute();
