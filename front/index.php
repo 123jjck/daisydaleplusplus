04282700c0cbf617c43b9d8108a3efd637c6fa4d
123jjck <48408304+123jjck@users.noreply.github.com>
2021-02-24 20:21:46 +0500

    Update index.php

--- front/index.php
+++ front/index.php
@@ -66,7 +66,7 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
                 	<input name='username'  placeholder='Логин' /><br/>
                 	<input name='password' type='password' placeholder='Пароль'  /><br/>
                 	<br/>
-                	<button class='meow-btn' type='submit' style="display: inline-block;" name='btnLogin'> Войти </button>  <a class='meow-btn' style='background:#ff8787; display: inline-block;' href='/register.php' name='btnReg'> Создать аккаунт </button>  <a/>
+                	<button class='meow-btn' type='submit' style="display: inline-block;" name='btnLogin'> Войти </button>  <a class='meow-btn' style='background:#ff8787; display: inline-block;' href='/register.php' name='btnReg'> Создать аккаунт </a>
 		</form>
 	</div>
 </body>
