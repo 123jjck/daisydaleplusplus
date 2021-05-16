dfbd74980d69e285f08b81a65f612f8cfbdc8bc3
123jjck <48408304+123jjck@users.noreply.github.com>
2021-05-16 12:15:35 +0500

    say goodbye to mysqli

--- front/db_connection.php
+++ front/db_connection.php
@@ -4,16 +4,6 @@ $host = 'localhost'; // Хост
 $user = 'root'; // Юзер
 $password = ''; // Пароль
 $dbname = 'daisy'; // Название базы данных
-try
-{
-    $db = new mysqli($host, $user, $password, $dbname);
-    if (!$db) throw new Exception('Connection error');
-}
-catch(Exception $e)
-{
-    echo 'Ошибка! Попробуйте зайти в следующий раз!';
-    die;
-}
 
 try
 {
