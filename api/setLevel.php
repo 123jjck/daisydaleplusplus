403f6af25953ab3f3c8f86681fe232341daeeda5
123jjck <48408304+123jjck@users.noreply.github.com>
2021-05-16 12:44:14 +0500

    advanced verify

--- api/setLevel.php
+++ api/setLevel.php
@@ -1,5 +1,5 @@
 <?php
-include("db_connection.php"); 
+require("db_connection.php"); 
 
 if(!(isset($_POST['level']) && isset($_POST['id']))) exit('error');
 if(mb_strlen($_POST["level"]) > 5) exit('error');
