2bf07efc1c86241f9913f60a054d664e078a84f1
123jjck <48408304+123jjck@users.noreply.github.com>
2021-08-30 19:34:43 +0500

    Update bg.php

--- api/bg.php
+++ api/bg.php
@@ -1,5 +1,5 @@
 <?php
 require_once("db_connection.php"); 
-$query = $dbh->prepare("UPDATE users SET BG  = :bg WHERE TICKET = :token");
+$query = $dbh->prepare("UPDATE users SET BG = :bg WHERE TICKET = :token");
 $query->execute(array('bg' => $_POST['bg'], 'token' => $_POST['ticket']));
 ?>
