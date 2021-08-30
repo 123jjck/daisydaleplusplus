e79a3f96927657def159516855ab99fbbdef0e34
123jjck <48408304+123jjck@users.noreply.github.com>
2021-08-30 19:34:52 +0500

    Update setAvatar.php

--- api/setAvatar.php
+++ api/setAvatar.php
@@ -1,5 +1,5 @@
 <?php
 require_once("db_connection.php"); 
-$query = $dbh->prepare("UPDATE users SET AVATAR  = :avatar WHERE TICKET = :token");
+$query = $dbh->prepare("UPDATE users SET AVATAR = :avatar WHERE TICKET = :token");
 $query->execute(array('avatar' => $_POST['avatar'], 'token' => $_POST['ticket']));
 ?>
