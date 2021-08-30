711f2473f025bdf6363ebcad8f3afe3989c75fd3
123jjck <48408304+123jjck@users.noreply.github.com>
2021-08-30 19:35:31 +0500

    Update setLevel.php

--- api/setLevel.php
+++ api/setLevel.php
@@ -1,5 +1,5 @@
 <?php
-require("db_connection.php"); 
+require_once("db_connection.php"); 
 
 if(!(isset($_POST['level']) && isset($_POST['id']))) exit('error');
 if(mb_strlen($_POST["level"]) > 5) exit('error');
