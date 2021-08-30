0a4c9127b790dfc2f71665fc6e88d06dda36a0aa
123jjck <48408304+123jjck@users.noreply.github.com>
2021-08-30 19:31:40 +0500

    Update addMoney.php

--- api/addMoney.php
+++ api/addMoney.php
@@ -1,6 +1,6 @@
 <?php
 require_once("db_connection.php"); 
-$query = $dbh->prepare("UPDATE users SET MONEY= MONEY + :amount WHERE TICKET = :ticket");
+$query = $dbh->prepare("UPDATE users SET MONEY = MONEY + :amount WHERE TICKET = :ticket");
 $query->bindParam('amount', $_POST['amount']);
 $query->bindParam('ticket', $_POST['ticket']);
 $query->execute();
