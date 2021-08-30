7e74b928ec2d8f8e3aed8db3fae195a79d574f56
123jjck <48408304+123jjck@users.noreply.github.com>
2021-08-30 19:31:01 +0500

    Update addGold.php

--- api/addGold.php
+++ api/addGold.php
@@ -1,6 +1,5 @@
 <?php
-include("db_connection.php"); 
-global $dbh;
+require_once("db_connection.php"); 
 $query = $dbh->prepare("UPDATE users SET GOLD = GOLD + :amount WHERE TICKET = :ticket");
 $query->bindParam('ticket', $_POST['ticket']);
 $query->bindParam('amount', $_POST['amount']);
