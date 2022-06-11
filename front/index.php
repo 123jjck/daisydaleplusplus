f56ecc6a1c007581eee7c5d4ece533d9db019d10
123jjck <48408304+123jjck@users.noreply.github.com>
2022-06-11 13:53:28 +0500

    Update index.php

--- front/index.php
+++ front/index.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 if (isset($_SESSION["userId"])) {
-    header("Location: /game.php");    
+    header("Location: /game.php");
+    exit;
 }
 
 function generateTicket() {
@@ -15,9 +16,9 @@ function generateTicket() {
     return $randomString;
 }
 
-require("db_connection.php"); 
+require_once("db_connection.php"); 
 
-if (isset($_POST["username"]) && isset($_POST["password"])) {
+if (isset($_POST["username"], $_POST["password"])) {
   $a = $dbh->prepare("SELECT ID, PASSWORD, ROLEFLAGS FROM users WHERE USERNAME = :uname");
   $a->bindParam('uname', $_POST['username']);
   $a->execute();
@@ -25,17 +26,17 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
   if (!$fetched) {
     $error = "Нет такого смешарика";
   } else {    
-    if (password_verify(md5($_POST["password"]), $fetched['PASSWORD'])) {
+    if (password_verify(md5($_POST["password"]), $fetched["PASSWORD"])) {
       $token = generateTicket();
-	    
+
       $updTik = $dbh->prepare("UPDATE users SET TICKET = :token WHERE USERNAME = :uname;");
       $updTik->bindParam('token', $token);
-      $updTik->bindParam('uname', $_POST['username']);
+      $updTik->bindParam('uname', $_POST["username"]);
       $updTik->execute();
-	    
-      $_SESSION["userId"] = $fetched["ID"];
+
+      $_SESSION["userId"] = (int)$fetched["ID"];
       $_SESSION["ticket"] = $token;
-      $_SESSION["roleflags"] = $fetched["ROLEFLAGS"];
+      $_SESSION["roleflags"] = (int)$fetched["ROLEFLAGS"];
       header("Location: /");
     } else {
       $error = "Неправильный пароль";
@@ -55,12 +56,12 @@ if (isset($_POST["username"]) && isset($_POST["password"])) {
 	<div class="loginbox">
 		<img class="logo" src="/logo.png">
 		<form action='' method='POST'>
-                	<p class="message"><?php if (isset($error)) { echo $error; } ?></p>
+            <p class="message"><?php if (isset($error)) echo $error; ?></p>
 			<h1>Вход</h1>
-                	<input name='username'  placeholder='Логин' /><br/>
-                	<input name='password' type='password' placeholder='Пароль'  /><br/>
-                	<br/>
-                	<button class='meow-btn' type='submit' style="display: inline-block;" name='btnLogin'>Войти</button>&nbsp;<a class='meow-btn' style='background:#ff8787; display: inline-block;' href='/register.php' name='btnReg'>Создать аккаунт</a>
+            <input name='username' placeholder='Логин' /><br/>
+            <input name='password' type='password' placeholder='Пароль'  /><br/>
+            <br/>
+            <button class='meow-btn' type='submit' style="display: inline-block;" name='btnLogin'>Войти</button>&nbsp;<a class='meow-btn' style='background:#ff8787; display: inline-block;' href='/register.php' name='btnReg'>Создать аккаунт</a>
 		</form>
 	</div>
 </body>
