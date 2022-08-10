a4c3f173c3bedaa3993ea39a99021e165ed90319
123jjck <48408304+123jjck@users.noreply.github.com>
2022-08-10 18:12:29 +0500

    Update fs.php

--- misc/minfs/fs.php
+++ misc/minfs/fs.php
@@ -1,45 +1,42 @@
 <?php
-
 /*
 	минимальный ресурсный сервер
 	(c) Дейзи, Jjck
 */
-
+if(!isset($_GET['filename'])) die;
 define('root','http://sharaball.ru/fs/');
 define('fname', $_GET['filename']);
-define('error', "Пошёл нафиг!");
 
 function validate_name($filename) {
-	if (strlen($filename) > 255) { // no mb_* since we check bytes
-		echo error;
-		exit;
-	}
+	if (strlen($filename) > 255) return false; // no mb_* since we check bytes
 	$invalidCharacters = '|\'\\?*&<";:>+[]=/';
-	if (strpbrk($filename, $invalidCharacters) !== false) {
-		echo error;
-		exit;
-	}
-
+	if (strpbrk($filename, $invalidCharacters) !== false) return false;
 	$path_info = pathinfo('./' . $filename);
-	if($path_info['extension'] !== 'swf' &&
-	$path_info['extension'] !== 'png' &&
-	$path_info['extension'] !== 'jpg') {
-		echo error;
-		exit;
-	}
+	if($path_info['extension'] !== 'swf' && $path_info['extension'] !== 'png' && $path_info['extension'] !== 'jpg') return false;
+	return true;
 }
 
 function sendBack($filename) {
-	header("Content-Description: File Transfer"); 
-	if(pathinfo('./' . $filename)['extension'] == 'swf') header("Content-Type: application/x-shockwave-flash");
-	if(pathinfo('./' . $filename)['extension'] == 'png') header("Content-Type: image/png");
-	if(pathinfo('./' . $filename)['extension'] == 'jpg') header("Content-Type: image/jpeg");
+	switch(pathinfo('./' . $filename)['extension']) {
+		case "swf":
+			$content_type = "application/x-shockwave-flash";
+			break;
+		case "png":
+			$content_type = "image/png";
+			break;
+		case "jpg":
+			$content_type = "image/jpeg";
+			break;
+		default:
+			$content_type = "application/octet-stream";
+	}
+	header("Content-Type: $content_type");
+
 	$size = filesize('./' . $filename);
 	header('Content-Length: ' . $size);
 	header("Cache-control: public");
 	header("Expires: " . gmdate("D, d M Y H:i:s", time() + 60*60*4) . " GMT");
-	exit(file_get_contents("./" . $filename));
-	//header("Location: /fs/" . $name);
+	readfile('./' . $filename);
 }
 
 function detect_encoding($string) { 
@@ -56,14 +53,9 @@ function detect_encoding($string) {
 function check_exists($filename) {
 	$filename = urldecode($filename); // на всякий случай 
 
-	if(detect_encoding($filename) == 'windows-1251') { // возможный фикс проблем с кодировкой на некоторых браузерах
-		$filename = iconv('Windows-1251', 'UTF-8', $filename);
-	}
-	
-	if (file_exists("./" . $filename)) {
-		sendBack($filename);
-		exit;
-	}
+	if(detect_encoding($filename) == 'windows-1251') $filename = iconv('Windows-1251', 'UTF-8', $filename); // возможный фикс проблем с кодировкой на некоторых браузерах
+
+	return file_exists("./" . $filename);
 }
 
 function check_404($filename) {
@@ -73,18 +65,13 @@ function check_404($filename) {
 	$res = curl_exec($ch);
 	$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-	if ($code == 404) {
-		http_response_code(404);
-		echo error;
-		exit;
-	}
+	return $code == 404; 
 }
 
 function download($filename) {
-	
 	$ch = curl_init();
-	$file = fopen("./" . fname, "w");
-	curl_setopt($ch, CURLOPT_URL, root . fname);
+	$file = fopen("./" . $filename, "w");
+	curl_setopt($ch, CURLOPT_URL, root . $filename);
 	curl_setopt($ch, CURLOPT_HEADER, 0);
 	curl_setopt($ch, CURLOPT_BUFFERSIZE, 65536);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
@@ -93,9 +80,14 @@ function download($filename) {
 	curl_exec($ch);
 }
 
-validate_name(fname);
-check_exists(fname);
-check_404(fname);
-download(fname);
-sendBack(fname);
+if(!validate_name(fname)) die;
+
+if(check_exists(fname)) {
+	sendBack(fname);
+} else if(!check_404(fname)) {
+	download(fname);
+	sendBack(fname);
+} else {
+	http_response_code(404);
+}
 ?>
