89753651012a6cd3756f81590a2e097f9210013a
123jjck <48408304+123jjck@users.noreply.github.com>
2022-08-10 18:16:36 +0500

    Update fs.php

--- misc/proxyfs/fs.php
+++ misc/proxyfs/fs.php
@@ -1,52 +1,52 @@
 <?php
+if(!isset($_GET['filename'])) die;
 $fname = $_GET["filename"];
 
 function validate_name($filename) {
-	if (strlen($filename) > 255) { // no mb_* since we check bytes
-		return false;
-	}
+	if (strlen($filename) > 255) return false; // no mb_* since we check bytes
 	$invalidCharacters = '|\'\\?*&<";:>+[]=/';
-	if (strpbrk($filename, $invalidCharacters) !== false) {
-		return false;
-	}
-
+	if (strpbrk($filename, $invalidCharacters) !== false) return false;
 	$path_info = pathinfo('./' . $filename);
-	if($path_info['extension'] !== 'swf' &&
-	$path_info['extension'] !== 'png' &&
-	$path_info['extension'] !== 'jpg') {
-		return false;
-	}
+	if($path_info['extension'] !== 'swf' && $path_info['extension'] !== 'png' && $path_info['extension'] !== 'jpg') return false;
 	return true;
 }
 
-if(!validate_name($fname)) exit("Пошёл нафиг!");
+if(!validate_name($fname)) die;
 
 if (file_exists("./" . $fname)) { //возврат файла, если он существует локально 
-	header("Content-Description: File Transfer"); 
-	if(pathinfo('./' . $fname)['extension'] == 'swf') header("Content-Type: application/x-shockwave-flash");
-	if(pathinfo('./' . $fname)['extension'] == 'png') header("Content-Type: image/png");
-	if(pathinfo('./' . $fname)['extension'] == 'jpg') header("Content-Type: image/jpeg");
+    switch(pathinfo('./' . $fname)['extension']) {
+		case "swf":
+			$content_type = "application/x-shockwave-flash";
+			break;
+		case "png":
+			$content_type = "image/png";
+			break;
+		case "jpg":
+			$content_type = "image/jpeg";
+			break;
+		default:
+			$content_type = "application/octet-stream";
+	}
+	header("Content-Type: $content_type");
+
 	$size = filesize('./' . $fname);
 	header('Content-Length: ' . $size);
 	header("Cache-control: public");
 	header("Expires: " . gmdate("D, d M Y H:i:s", time() + 60*60*4) . " GMT");
 
-	exit(file_get_contents("./" . $fname));
+	readfile('./' . $fname);
 } else {
     $root = "http://sharaball.ru/fs/";
     $url = $root . $fname; 
     $headers = get_headers($url, 1);
     $type = $headers["Content-Type"];
 
-	header("Content-Description: File Transfer");
 	header('Content-Type: ' . $type);
 	header('Content-Length: ' . $headers["Content-Length"]);
 	header("Cache-control: public");
 	header("Expires: " . gmdate("D, d M Y H:i:s", time() + 60*60*4) . " GMT");
 
-	if(($content = @file_get_contents($url)) === FALSE) {
-		exit("Пошёл нафиг!");
-	}
+	if(($content = @file_get_contents($url)) === FALSE) die;
     exit($content);
 }
 ?>
